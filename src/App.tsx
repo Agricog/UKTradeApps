f6661931ae898a3a8f5347c300946ecfdf1e48fb
Agricog <mick@mickstevenson.com>
2026-03-05 13:33:13 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import * as Sentry from '@sentry/react'
 import HomePage from './pages/HomePage'
 import TradeCategoryPage from './pages/TradeCategoryPage'
 import ListingPage from './pages/ListingPage'
+import QuizPage from './pages/QuizPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes)
@@ -14,14 +15,14 @@ export default function App() {
         {/* Public pages */}
         <Route path="/" element={<HomePage />} />
 
+        {/* Quiz — must be before /:tradeSlug */}
+        <Route path="/quiz" element={<QuizPage />} />
+
         {/* Trade category pages */}
         <Route path="/:tradeSlug" element={<TradeCategoryPage />} />
 
         {/* Individual listing page */}
         <Route path="/:tradeSlug/:listingSlug" element={<ListingPage />} />
-
-        {/* Quiz (Phase 4) */}
-        {/* <Route path="/quiz" element={<QuizPage />} /> */}
 
         {/* Vendor submission (Phase 5) */}
         {/* <Route path="/submit" element={<SubmitPage />} /> */}
